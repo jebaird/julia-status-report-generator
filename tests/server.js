456b0b67e3a62335f48918a5ba392ed6c9689741
Jesse Baird <jebaird@gmail.com>
2016-07-10 11:33:03 -0500

    udpated print styles

--- tests/server.js
+++ tests/server.js
@@ -3,6 +3,9 @@ var server=require('node-http-server');
 var config = new server.Config;
 
 config.contentType['tag'] = config.contentType['html'];
+config.contentType['woff2'] = config.contentType['font/woff2'];
+config.contentType['woff'] = config.contentType['font/woff'];
+config.contentType['ttf'] = config.contentType['font/ttf'];
 
 config.port = 8000;
 config.root = './'
